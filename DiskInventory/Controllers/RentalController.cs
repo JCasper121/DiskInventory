cf851b0dc2f136a1ea20dc0733041cebeabab1ac
jcasperGIT <jacqobin@gmail.com>
2021-04-30 15:08:39 -0600

    Add/Edit Rentals

---
+++ DiskInventory/Controllers/RentalController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiskInventory.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DiskInventory.Controllers
+{
+    public class RentalController : Controller
+    {
+        private disk_inventoryJCContext context { get; set; }
+
+        public RentalController(disk_inventoryJCContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            var rentals = context.DiskRentals.Include(r => r.Disk).Include(r => r.Borrower).OrderBy(r => r.BorrowedDate).ThenBy(r => r.DueDate).ToList();
+            //var rentals = context.DiskRentals.ToList();
+
+            return View(rentals);
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Disks = context.Disks.OrderBy(d => d.DiskName).ToList();
+            ViewBag.Borrowers = context.Borrowers.OrderBy(b => b.LastName).ToList();
+            DiskRental rental = new DiskRental();
+            return View("Edit", rental);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Disks = context.Disks.OrderBy(d => d.DiskName).ToList();
+            ViewBag.Borrowers = context.Borrowers.OrderBy(b => b.LastName).ToList();
+            var rental = context.DiskRentals.Find(id);
+            return View(rental);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult Edit(DiskRental rental)
+        {
+            if(rental.RentalId == 0)
+            {
+                context.DiskRentals.Add(rental);
+            } else
+            {
+                context.DiskRentals.Update(rental);
+
+            }
+            context.SaveChanges();
+            return RedirectToAction("Index", "Rental");
+
+        }
+    }
+}
--- DiskInventory/Models/DiskRental.cs
+++ DiskInventory/Models/DiskRental.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,10 +9,16 @@ namespace DiskInventory.Models
     public partial class DiskRental
     {
         public int RentalId { get; set; }
+        [Required(ErrorMessage = "Please enter a date.")]
         public DateTime BorrowedDate { get; set; }
+        [Required(ErrorMessage = "Please enter a date.")]
         public DateTime DueDate { get; set; }
+        //[Required(ErrorMessage = "Please enter a date.")]
         public DateTime? ReturnDate { get; set; }
+        [Required(ErrorMessage = "Please select a borrower.")]
         public int BorrowerId { get; set; }
+
+        [Required(ErrorMessage = "Please select a disk.")]
         public int DiskId { get; set; }
 
         public virtual Borrower Borrower { get; set; }
